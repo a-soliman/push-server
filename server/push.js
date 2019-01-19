815e9448f3be1212c44e9189a19dd2a6ab1bf764
a-soliman <ahmed.soliman@programmer.net>
2019-01-19 12:00:49 -0800

    push module

---
+++ server/push.js
@@ -0,0 +1,13 @@
+const webpush = require("web-push");
+const urlsafeBase64 = require("urlsafe-base64");
+const vapid = require("./vapid.json");
+
+let subscriptions = [];
+
+// Create URL sage vapid
+module.exports.getKey = () => urlsafeBase64.decode(vapid.publicKey);
+
+module.exports.addSubscription = subscription => {
+  // Add to subscriptions array;
+  subscriptions.push(subscription);
+};
